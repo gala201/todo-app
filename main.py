afc33b97348e8215d3a8a08185aa85c2f266362c
Ivan Galić <ivan.igalic.gali@gmail.com>
2025-04-07 20:41:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from modules.functions import get_todos, write_todos
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    # Get user input and strip space chars from it
+    user_action = input("Type add, show, edit, complete or exit:")
+    user_action = user_action.strip()
+
+    # Check if user action is "add"
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + '\n')
+
+        # Write todos in a file
+
+        write_todos("todos.txt", todos)
+
+    elif user_action.startswith("show"):
+
+        todos = get_todos("todos.txt")
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("enter new todo: ")
+            todos[number] = new_todo + '\n'
+
+            write_todos("todos.txt", todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos( )
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos("todos.txt", todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+    else:
+        print("Invalid command.")
+
+
+print("Bye!")
+
+
